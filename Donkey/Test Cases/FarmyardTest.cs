5746bbe912ab0987f80de5d07d9adf76045b2664
David <dchong@globalnet.co.uk>
2016-07-19 14:16:25 +0100

    Added test cases

---
+++ Donkey/Test Cases/FarmyardTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Donkey.Test_Cases
+{
+    [TestFixture]
+    public class FarmyardTest
+    {
+        [Test]
+        public void TestCat()
+        {
+            Farmyard fy = new Farmyard();
+
+            String sound = fy.PlaySound("cat");
+
+            Assert.AreEqual("meow", sound);
+        }
+
+        [Test]
+        public void TestDog()
+        {
+            Farmyard fy = new Farmyard();
+
+            String sound = fy.PlaySound("dog");
+
+            Assert.AreEqual("woof", sound);
+        }
+
+        [Test]
+        public void TestDonkey()
+        {
+            Farmyard fy = new Farmyard();
+
+            String sound = fy.PlaySound("donkey");
+
+            Assert.AreEqual("pfft", sound);
+        }
+    }
+}
